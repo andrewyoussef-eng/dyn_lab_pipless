2f5cb57d57423190adf0c2e7e589b45db777d3bd
andrewyoussef-eng <83728910+andrewyoussef-eng@users.noreply.github.com>
2021-05-13 23:09:11 +0200

    Update Test.py

--- Script/Test.py
+++ Script/Test.py
@@ -11,8 +11,12 @@ from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 rospy.init_node('test', anonymous=True)
+# global cmd_pub1
 cmd_pub1 = rospy.Publisher("/"+str(name)+"/cmd_vel", Twist,queue_size=10)
 global yaw
+global cur_pos
+global theta1
+global nr_obst
 global stuck
 flag_initial_Pos = 0	#Initialize flag by zero
 xcordinit = 0 
@@ -34,6 +38,8 @@ thetayaw3 = 0
 xcord4 = 0
 ycord4 = 0
 thetayaw4 = 0
+cur_pos = [xcord,ycord,xcord2,ycord2,xcord3,ycord3,xcord4,ycord4] # x and y positions of robots
+theta1 = [thetayaw,thetayaw2,thetayaw3,thetayaw4] # theta orientations of the robots
 theta_des=0
 yaw=0
 ##Callback function for feedback the vehicle current position
@@ -46,15 +52,15 @@ def callback(data):
   global ycord
   
   #pos_msg = data				#Initialize pos_msg with data sent to the function
-  xcord = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
-  ycord = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
+  cur_pos[0] = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
+  cur_pos[1] = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
   quaternion = (data.pose.pose.orientation.x,
 		data.pose.pose.orientation.y,
 		data.pose.pose.orientation.z,
 		data.pose.pose.orientation.w)
   euler = euler_from_quaternion(quaternion)
-  yaw = euler[2]
-  thetayaw = round(yaw, 4)	#Round the value of theta to 4 decimal places
+  theta1[0] = euler[2]
+  #theta[0] = round(yaw, 4)	#Round the value of theta to 4 decimal places
 if name == "robot1":  
   sub2 = rospy.Subscriber("/robot1/odom", Odometry, callback) #Identify the subscriber "sub2" to subscribe topic "/turtle1/pose" of type "Pose"
 else:
@@ -70,15 +76,15 @@ def callforward(data):
   global ycord2
   
   #pos_msg = data				#Initialize pos_msg with data sent to the function
-  xcord2 = round(data.pose.pose.position.x, 4)	#Round the value of x to 4 decimal places
-  ycord2 = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
+  cur_pos[2] = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
+  cur_pos[3] = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
   quaternion = (data.pose.pose.orientation.x,
 		data.pose.pose.orientation.y,
 		data.pose.pose.orientation.z,
 		data.pose.pose.orientation.w)
   euler = euler_from_quaternion(quaternion)
-  yaw_3 = euler[2]
-  thetayaw2 = round(yaw, 4)	#Round the value of theta to 4 decimal places
+  theta1[1] = euler[2]
+  #theta[1] = round(yaw, 4)	#Round the value of theta to 4 decimal places
 if name == "robot2":
   sub1 = rospy.Subscriber("/robot1/odom", Odometry, callforward) #Identify the subscriber "sub2" to subscribe topic "/turtle1/pose" of type "Pose"
 else:
@@ -92,15 +98,16 @@ def callback3(data):
   global xcord3
   global ycord3
   
-  xcord3 = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
-  ycord3 = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
-  quaternion3 = (data.pose.pose.orientation.x,
+  #pos_msg = data				#Initialize pos_msg with data sent to the function
+  cur_pos[4] = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
+  cur_pos[5] = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
+  quaternion = (data.pose.pose.orientation.x,
 		data.pose.pose.orientation.y,
 		data.pose.pose.orientation.z,
 		data.pose.pose.orientation.w)
-  euler3 = euler_from_quaternion(quaternion3)
-  yaw3 = euler3[2]
-  thetayaw3 = round(yaw3, 4)	#Round the value of theta to 4 decimal places
+  euler = euler_from_quaternion(quaternion)
+  theta1[2] = euler[2]
+  #theta[2] = round(yaw, 4)	#Round the value of theta to 4 decimal places
 if name == "robot3":
   sub3 = rospy.Subscriber("/robot1/odom", Odometry, callback3) #Identify the subscriber "sub2" to subscribe topic "/turtle1/pose" of type "Pose"
 else:
@@ -115,19 +122,23 @@ def callback4(data):
   global xcord4
   global ycord4
   
-  xcord4 = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
-  ycord4 = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
-  quaternion4 = (data.pose.pose.orientation.x,
+  #pos_msg = data				#Initialize pos_msg with data sent to the function
+  cur_pos[6] = round(data.pose.pose.position.x, 4)		#Round the value of x to 4 decimal places
+  cur_pos[7] = round(data.pose.pose.position.y, 4)		#Round the value of y to 4 decimal places
+  quaternion = (data.pose.pose.orientation.x,
 		data.pose.pose.orientation.y,
 		data.pose.pose.orientation.z,
 		data.pose.pose.orientation.w)
-  euler4 = euler_from_quaternion(quaternion4)
-  yaw4 = euler4[2]
-  thetayaw4 = round(yaw4, 4)	#Round the value of theta to 4 decimal places
+  euler = euler_from_quaternion(quaternion)
+  theta1[3] = euler[2]
+  #theta[3] = round(yaw, 4)	#Round the value of theta to 4 decimal places
 if name == "robot4":
   sub4 = rospy.Subscriber("/robot1/odom", Odometry, callback4) #Identify the subscriber "sub2" to subscribe topic "/turtle1/pose" of type "Pose"
 else:
   sub4 = rospy.Subscriber("/robot4/odom", Odometry, callback4) #Identify the subscriber "sub2" to subscribe topic "/turtle1/pose" of type "Pose"
+
+
+nr_obst = 3
 global cmd_msg1
 cmd_msg1 = Twist()
 
